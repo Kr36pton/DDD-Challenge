86dabd6366ad0fa13efe61b54c54b24720336073
Kr36pton <jmmesa37@misena.edu.co>
2022-05-20 18:18:51 -0500

    Se agregan atributos del AR

--- domain/src/main/java/co/com/sofka/domicilio/domicilio/Domicilio.java
+++ domain/src/main/java/co/com/sofka/domicilio/domicilio/Domicilio.java
@@ -4,7 +4,16 @@ import co.com.sofka.domain.generic.AggregateEvent;
 import co.com.sofka.domicilio.domicilio.values.*;
 
 public class Domicilio extends AggregateEvent{
-    public Domicilio(DomicilioId entityId){
+    protected final DomicilioId domicilioId;
+    protected final Cliente cliente;
+    protected final Pedido pedido;
+    protected final Valor valor;
+
+    public Domicilio(DomicilioId entityId, DomicilioId domicilioId, Cliente cliente, Pedido pedido, Valor valor) {
         super(entityId);
+        this.domicilioId = domicilioId;
+        this.cliente = cliente;
+        this.pedido = pedido;
+        this.valor = valor;
     }
 }
---
+++ domain/src/main/java/co/com/sofka/domicilio/domicilio/Pedido.java
@@ -0,0 +1,5 @@
+package co.com.sofka.domicilio.domicilio;
+
+public class Pedido {
+    
+}
