a6cec00ae7f970127d7ff814754ca456cba9ed77
Kr36pton <jmmesa37@misena.edu.co>
2022-05-20 19:33:31 -0500

    Agregado Orientado a eventos

--- domain/src/main/java/co/com/sofka/domicilio/domicilio/Domicilio.java
+++ domain/src/main/java/co/com/sofka/domicilio/domicilio/Domicilio.java
@@ -1,19 +1,32 @@
 package co.com.sofka.domicilio.domicilio;
 
+import java.util.List;
+
 import co.com.sofka.domain.generic.AggregateEvent;
+import co.com.sofka.domain.generic.DomainEvent;
 import co.com.sofka.domicilio.domicilio.values.*;
+import co.com.sofka.domicilio.domicilio.events.*;
 
 public class Domicilio extends AggregateEvent<DomicilioId>{
-    protected final DomicilioId domicilioId;
-    protected final Cliente cliente;
-    protected final Pedido pedido;
-    protected final Valor valor;
+    protected DomicilioId domicilioId;
+    protected Cliente cliente;
+    protected Pedido pedido;
+    protected Valor valor;
 
     public Domicilio(DomicilioId entityId, DomicilioId domicilioId, Cliente cliente, Pedido pedido, Valor valor) {
         super(entityId);
-        this.domicilioId = domicilioId;
-        this.cliente = cliente;
-        this.pedido = pedido;
-        this.valor = valor;
+        appendChange(new DomicilioCreado(cliente, pedido, valor)).apply();
+        subscribe(new DomicilioEventChange(this));
+    }
+
+    private Domicilio(DomicilioId entityId){
+        super(entityId);
+        subscribe(new DomicilioEventChange(this));
+    }
+
+    public static Domicilio from(DomicilioId entityId, List<DomainEvent> events){
+        var domicilio = new Domicilio(entityId);
+        events.forEach(domicilio::applyEvent);
+        return domicilio;
     }
 }
